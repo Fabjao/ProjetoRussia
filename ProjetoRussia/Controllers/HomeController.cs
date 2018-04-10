497bee49492e9fee662c352ea9c3bd1e392ef8ee
Fabio Gonçalves <fabiobasquete4@hotmail.com>
2018-04-09 23:45:35 -0300

    Criado o projeto class libere

--- ProjetoRussia/Controllers/HomeController.cs
+++ ProjetoRussia/Controllers/HomeController.cs
@@ -14,21 +14,11 @@ namespace ProjetoRussia.Controllers
         {
             return View();
         }
-
-        public IActionResult About()
+         
+        public IActionResult Teste()
         {
-            ViewData["Message"] = "Your application description page.";
-
             return View();
         }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
